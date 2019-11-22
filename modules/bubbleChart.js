08881da31def76a7c2ba8fe364b7de2fabe8b88a
Roy Kuijper <roykuijper199716@hotmail.com>
2019-11-22 16:23:14 +0100

    Displayed the counts on the bubbles

--- modules/bubbleChart.js
+++ modules/bubbleChart.js
@@ -1,29 +1,31 @@
 export default function createCircles(data) {
     // Set standards
-    const width = '1800';
-    const height = '800';
+    const width = '1000';
+    const height = '1000';
     const format = d3.format(',d');
     // Give each different category their own color
     var color = d3.scaleOrdinal(d3.schemeCategory20);
     
     const pack = data => d3.pack()
-        .size([width - 2, height - 2])
-        .padding(5)
+        .size([width, height])
         (d3.hierarchy({children: data})
             .sum(d => d.categoryAmount));
 
     const root = pack(data);
     // Select svg
-    const svg = d3.selectAll('svg')
+    const svg = d3.selectAll('.visualizationWrap')
+        .append('svg')
         .attr('viewBox', [0, 0, width, height])
         .append('g')
-        .attr('class', 'circle__container');
+        .attr('class', 'circle__container')
+        .attr('transform', d => `translate(500)`);
     // Create parent element for the circles and text
     const leaf = svg.selectAll('g')
         .data(root.leaves())
         .enter()
         .append('g')
         .attr('transform', d => `translate(${d.x + 1},${d.y + 1})`);
+
     // Create bubbles
     leaf.append('circle')
         .attr('id', d => d.id)
@@ -34,10 +36,26 @@ export default function createCircles(data) {
     leaf.append('text')
         .attr('x', 0)
         .attr('y', 0)
+        .style("font-size", "2em")
+        .attr("font-family",  "Montserrat")
+        .style("text-anchor", "middle")
         .attr('fill', 'white')
     // Make text invisible when the amount of the category is lower than 500
         .attr('display', d => {return d.data.categoryAmount <= 500 ? 'none' : 'flex';})
         .text(d => d.data.categoryName);
+
+    leaf.append("text")
+        .attr("dy", "1.3em")
+        .style("text-anchor", "middle")
+        .attr('display', d => {return d.data.categoryAmount <= 500 ? 'none' : 'flex';})
+        .text(function(d) {
+            return d.data.categoryAmount;
+        })
+        .attr("font-family",  "Gill Sans", "Gill Sans MT")
+        .attr("font-size", function(d){
+            return d.r/5;
+        })
+        .attr("fill", "white");
     // Add hover effect with data
     leaf.append('title')
         .text(d => `${d.data.categoryName}\n${format(d.data.categoryAmount)}\nCategorie: ${d.data.mainCategory}`);
